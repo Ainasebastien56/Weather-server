f8d207b91acbba666b0a880f6daf7592401f4f30
Sébastien Randriamanana <sebastienrandriamanana3@gmail.com>
2024-08-30 10:51:16 +0300

    latest commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const axios = require("axios")
 
 const app = express();
-const port = process.nextTick.PORT || 3000;
+const port = process.env.PORT || 3000;
 const newsAPIKey = process.env.NEWS_API_KEY;
 
 app.use(cors());
